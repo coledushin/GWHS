71e82950f4649f7f217200bc6c4f709133dfadaa
coledushin <coledushin@gmail.com>
2026-05-31 09:00:59 -0400

    Edit to generate all to make it cast a full net

--- generate_all.sh
+++ generate_all.sh
@@ -2,7 +2,6 @@
 set -e
 cd "$(dirname "$0")"
 
-python3 "Global/April CMA/generate_report.py"
-python3 "Global/May Final/generate_report.py"
-python3 "US/April CMA/generate_report.py"
-python3 "US/May Final/generate_report.py"
+find . -name "generate_report.py" | sort | while read -r script; do
+    python3 "$script"
+done
